177211a2760be4e4669f8ce636000b8a5de6aa48
Vladislaff Evtushenko <starson4587@gmail.com>
2015-10-29 20:58:42 +0300

    - Client-Server Socket Demo App added - Next step is recreate Socket Server project - I cancelled creating add robot interface because it is difficult and may not be necessary at all.

---
+++ SocketClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Socket.SocketClient
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      SynchronousSocketClient.StartClient();
+    }
+  }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Socket.SocketServer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      SynchronousSocketListener.StartListening();
+    }
+  }
+}
