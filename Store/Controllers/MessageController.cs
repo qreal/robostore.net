cb3de5876c9b7b65e7ae80ee7b460fbadf11d991
Vladislaff Evtushenko <starson4587@gmail.com>
2015-11-18 21:54:54 +0300

    - Triangle Trade : сервер отправляет запрос сайту и получает ответ

---
+++ Store/Controllers/MessageController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Store.Controllers
+{
+  public class MessageController : Controller
+  {
+
+    // POST: api/Values
+    [HttpPost]
+    public JsonResult Post(string value)
+    {
+      HomeController.Messages.Add(value);
+      return Json("success");
+    }
+
+  }
+}
