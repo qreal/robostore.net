34231369d8b0af553cee68949e143d2cd9c887dc
Vlad Evtushenko <cryptically@yandex.ru>
2016-03-17 14:59:42 +0300

    - добавил страницу регистрации - обновил схемы - с регистрацией проблема с автопоиском страницы

---
+++ Store/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Domain.Data;
+using Domain.Users;
+using Store.Models.User;
+
+namespace Store.Controllers
+{
+  public class UserController : Controller
+  {
+    private UserManager userManager;
+
+    public UserController(IData d)
+    {
+      userManager = new UserManager(d);
+    }
+
+    // GET: User
+    public ActionResult Registration()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Registration(UserProfile profile)
+    {
+      // если пароль и логин введены
+      if (ModelState.IsValid && !userManager.TryEnter(profile.Login, profile.Password))
+      {
+        await userManager.CreateUser(profile.Login, profile.Password);
+        return View("Welcome", profile.Login);
+      }
+      return View();
+    }
+  }
+}
---
+++ Store/Models/User/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Models.User
+{
+  public class UserProfile
+  {
+    [Required]
+    public string Login { get; set; }
+    [Required]
+    public string Password { get; set; }
+  }
+}
