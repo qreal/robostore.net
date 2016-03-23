95ca3dcf73e5f43fd10499826c8e84bc7ac4f501
Vlad Evtushenko <cryptically@yandex.ru>
2016-03-23 22:16:11 +0300

    - Переписал код левого меню с c# на jquery.

--- Store/Scripts/Menu/Menu.js
+++ Store/Scripts/Menu/Menu.js
@@ -4,46 +4,24 @@
 */
 
 /*
-Выделить кнопку цветом
+Выделить кнопку(ссылку) цветом
 */
 function setItemSelected(id) {
     $("#leftMenuItem" + id).addClass("btn-primary");
 }
 
-function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
-}
-
-function onClickCategory(id) {
-    $("#leftMenuItem" + id).click(function () {
-        deleteAllCookies();
-        /*
-            Важно, что path определяет адрес, где доступны cookie
-            я поставил, чтобы были доступны везде
-        */
-        document.cookie = "selectedMenuItemId=" + id + ";path=/";
-    });
-}
-
 $(document).ready(function () {
-    var id = getCookie("selectedMenuItemId");
-    setItemSelected(id);
+    setItemSelected(getCookie("selectedMenuItemId"));
+
+    /*
+        По клику ссылки будут перезаписывать в cookie выбранную категорию
+    */
+    var links = $('a[id^="leftMenuItem"]');
+    links.each(function () {
+        var number = (this.id).replace('leftMenuItem', '');
+        $(this).on("click", function () {
+            deleteAllCookies();
+            document.cookie = "selectedMenuItemId=" + number + ";path=/";
+        });
+    });
 });
